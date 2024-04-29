9ef508c1b6e8f019d62c66a5b548df3ab8113b8c
minhyuk <dbd05088@naver.com>
2024-04-29 21:22:10 +0900

    subsample aqua&hrvqa

--- create_random_data.py
+++ create_random_data.py
@@ -9,36 +9,31 @@ with open("./dataset/AQUA/train/dataset.json") as fp:
 with open("./dataset/HRVQA-1.0/train/dataset.json") as fp:
     hrvqa = json.load(fp)
     
-with open("./dataset/VQA-MED/train/dataset.json") as fp:
-    vqamed = json.load(fp)
+# with open("./dataset/VQA-MED/train/dataset.json") as fp:
+#     vqamed = json.load(fp)
 
 print(len(aqua))
 print(len(hrvqa))
-print(len(vqamed))
+# print(len(vqamed))
 # breakpoint()
 
-size = 40000
+size = 60000
 indices = np.arange(len(aqua)).tolist()
 selection = sorted(random.sample(indices, size))
 new_list = []
 for idx in selection:
     new_list.append(aqua[idx])
 with open("./scenarios/AQUA-0.json", 'w') as json_file:
-    json.dump(new_list[:8000], json_file, indent=4)
+    json.dump(new_list[:20000], json_file, indent=4)
 
 with open("./scenarios/AQUA-1.json", 'w') as json_file:
-    json.dump(new_list[8000:16000], json_file, indent=4)
+    json.dump(new_list[20000:40000], json_file, indent=4)
     
 with open("./scenarios/AQUA-2.json", 'w') as json_file:
-    json.dump(new_list[16000:24000], json_file, indent=4)
+    json.dump(new_list[40000:60000], json_file, indent=4)
 
-with open("./scenarios/AQUA-3.json", 'w') as json_file:
-    json.dump(new_list[24000:32000], json_file, indent=4)
 
-with open("./scenarios/AQUA-4.json", 'w') as json_file:
-    json.dump(new_list[32000:40000], json_file, indent=4)
-
-size = 40000
+size = 120000
 indices = np.arange(len(hrvqa)).tolist()
 selection = sorted(random.sample(indices, size))
 new_list = []
@@ -46,19 +41,19 @@ for idx in selection:
     new_list.append(hrvqa[idx])
 
 with open("./scenarios/HRVQA-1.0-0.json", 'w') as json_file:
-    json.dump(new_list[:8000], json_file, indent=4)
+    json.dump(new_list[:20000], json_file, indent=4)
 
 with open("./scenarios/HRVQA-1.0-1.json", 'w') as json_file:
-    json.dump(new_list[8000:16000], json_file, indent=4)
+    json.dump(new_list[40000:60000], json_file, indent=4)
     
 with open("./scenarios/HRVQA-1.0-2.json", 'w') as json_file:
-    json.dump(new_list[16000:24000], json_file, indent=4)
+    json.dump(new_list[60000:80000], json_file, indent=4)
 
 with open("./scenarios/HRVQA-1.0-3.json", 'w') as json_file:
-    json.dump(new_list[24000:32000], json_file, indent=4)
+    json.dump(new_list[80000:100000], json_file, indent=4)
 
 with open("./scenarios/HRVQA-1.0-4.json", 'w') as json_file:
-    json.dump(new_list[32000:40000], json_file, indent=4)
+    json.dump(new_list[100000:120000], json_file, indent=4)
     
 # size = 12000
 # indices = np.arange(len(vqamed)).tolist()
