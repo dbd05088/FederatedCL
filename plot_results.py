f2db3c44581260d0f322f8977611e22e6693f590
mounKim <nasmik419@gmail.com>
2024-05-05 09:55:20 +0900

    change plotting to current eval method

--- plot_results.py
+++ plot_results.py
@@ -2,137 +2,87 @@ import os
 import numpy as np
 import warnings
 import matplotlib.pyplot as plt
-import json
+import copy
 warnings.filterwarnings(action='ignore')
 
-plot_type = 'rouge_l' # loss, precision, bleu, meteor, rouge_l, cider
-target_method_name = 'fedavg'
-dir = f'./results/{target_method_name}/{target_method_name}/'
+plot_types = ['rouge_l', 'recall', 'precision', 'bleu', 'meteor', 'rouge_l', 'cider']
+Method = 'fedavg_bunny3b'
 num_clients = 10
+num_rounds = 10
+
+target_dirs = [
+    './eval_results/fedavg/fedavg_eval/round_1.log',
+    './eval_results/fedavg/fedavg_eval_round2/round_2.log',
+    './eval_results/fedavg/fedavg_eval_round3/round_3.log',
+    './eval_results/fedavg/fedavg_eval_round4/round_4.log',
+    './eval_results/fedavg/fedavg_eval_round5/round_5.log',
+    './eval_results/fedavg/fedavg_eval_round6/round_6.log',
+    './eval_results/fedavg/fedavg_eval_round7/round_7.log',
+    './eval_results/fedavg/fedavg_eval_round8/round_8.log',
+    './eval_results/fedavg/fedavg_eval_round9/round_9.log',
+    './eval_results/fedavg/fedavg_eval_round10/round_10.log',
+]
+
+datalist = ['HRVQA-0','HRVQA-1','HRVQA-2','HRVQA-3','HRVQA-4','HRVQA-5','HRVQA-6','HRVQA-7','HRVQA-8','HRVQA-9',]
 
 
-def print_from_log(filename):
-    samples = []
-    loss = []
-    precision = []
-    bleu = []
-    meteor = []
-    rouge_l = []
-    cider = []
-    # with open(exp_name+'/metrics.json', 'r') as fp:
-    #     metrics = json.loads(fp)
-    fp = open(filename+'.log', 'r')
+single_dict = {'rounds':[],'precision':[], 'recall':[],
+            'bleu':[],'meteor':[],'rouge_l':[],'cider':[]}
+clients = {id:copy.deepcopy(single_dict) for id in range(num_clients)}
+server = {dataname:copy.deepcopy(single_dict) for dataname in datalist}
+
+for round, filename in enumerate(target_dirs):
+    fp = open(filename, 'r')
     lines = fp.readlines()
     fp.close()
     for line in lines:
         if "Test" in line:
             line_s = line.split(" ")
-            samples.append(int(line_s[7]))
-            loss.append(float(line_s[13]))
-            precision.append(float(line_s[16]))
-            bleu.append(float(line_s[19][1:-1]))
-            meteor.append(float(line_s[30][1:-1]))
-            rouge_l.append(float(line_s[33][1:-1]))
-            cider.append(float(line_s[36][1:-1]))
-    return {
-        'samples': samples,
-        'loss': loss,
-        'precision':precision,
-        'bleu':bleu,
-        'meteor':meteor,
-        'rouge_l':rouge_l,
-        'cider':cider,
-    }
+            if 'Client' in line:
+                id = int(line_s[6][:1])
+                clients[id]['rounds'].append(round+1)
+                clients[id]['precision'].append(float(line_s[12]))
+                clients[id]['recall'].append(float(line_s[15]))
+                clients[id]['bleu'].append(float(line_s[18][1:-1]))
+                clients[id]['meteor'].append(float(line_s[29][1:-1]))
+                clients[id]['rouge_l'].append(float(line_s[32][1:-1]))
+                clients[id]['cider'].append(float(line_s[35][1:-1]))
+            elif 'Server' in line:
+                dataname = line_s[7]
+                server[dataname]['rounds'].append(round+1)
+                server[dataname]['precision'].append(float(line_s[10]))
+                server[dataname]['recall'].append(float(line_s[13]))
+                server[dataname]['bleu'].append(float(line_s[16][1:-1]))
+                server[dataname]['meteor'].append(float(line_s[27][1:-1]))
+                server[dataname]['rouge_l'].append(float(line_s[30][1:-1]))
+                server[dataname]['cider'].append(float(line_s[33][1:-1]))
+                
 
-for client_id in range(num_clients):
-    if client_id in [7,8,9]:
-        continue
-    results_dict = print_from_log(dir + str(client_id) + '_client')
-
+for plot_type in plot_types:
+    # clients
+    plt.clf()
+    for client_id in range(num_clients):
+        plt.plot(clients[client_id]['rounds'], clients[client_id][plot_type], label=str(client_id), linewidth=2.0)
     
-    plt.plot(results_dict['samples'], results_dict[plot_type], label=str(client_id), linewidth=2.0)
-    # plt.plot(samples, precision, label=str(client_id), linewidth=2.0)
+    for i in range(num_rounds+1):
+        plt.axvline(x=i, color='r', linestyle='--', linewidth=0.5)
+    plt.xticks(list(range(num_rounds+1)))
+    plt.xlabel("# of rounds", fontsize=20)
+    plt.ylabel(plot_type, fontsize=20)
+    plt.legend()
+    plt.title(f"{Method} clients - {plot_type}", fontsize=20)
+    plt.savefig(f"{Method}_clients_{plot_type}.png")
+    # servr
+    plt.clf()
+    for dataname, server_info in server.items():
+        plt.plot(server_info['rounds'], server_info[plot_type], label=dataname, linewidth=2.0)
     
-# server results
-server_results = {}
-datanames = ['AQUA', 'HRVQA-1.0', 'VQA-MED']
-
-fp = open(dir + "server.log", 'r')
-lines = fp.readlines()
-fp.close()
-
-for dataname in datanames:
-    iter = 1
-    if dataname == 'AQUA':
-        samples = [0]
-        loss = [6.3117]
-        precision = [0.3831]
-        bleu = [0.251941]
-        meteor = [0.0621259]
-        rouge_l = [0.141933]
-        cider = [0.3564348]
-    elif dataname == 'HRVQA-1.0':
-        samples = [0]
-        loss = [8.4556]
-        precision = [0.3643]
-        bleu = [0.0319448227]
-        meteor = [0.034045510227]
-        rouge_l = [0.035888372]
-        cider = [0.087176975]
-    elif dataname == 'VQA-MED':
-        samples = [0]
-        loss = [5.9314]
-        precision = [0.4690]
-        bleu = [0.124068479]
-        meteor = [0.0335561]
-        rouge_l = [0.076864]
-        cider = [0.169718066]
-    else:
-        samples = []
-        loss = []
-        precision = []
-        bleu = []
-        meteor = []
-        rouge_l = []
-        cider = []
-    for line in lines:
-        if "Test" in line and dataname in line:
-            line_s = line.split(" ")
-            samples.append(int(800*iter))
-            loss.append(float(line_s[7]))
-            precision.append(float(line_s[10]))
-            bleu.append(float(line_s[13][1:-1]))
-            meteor.append(float(line_s[24][1:-1]))
-            rouge_l.append(float(line_s[27][1:-1]))
-            cider.append(float(line_s[30][1:-1]))
-            iter+=1
-    server_results[dataname] = {
-        'samples': samples,
-        'loss': loss,
-        'precision':precision,
-        'bleu':bleu,
-        'meteor':meteor,
-        'rouge_l':rouge_l,
-        'cider':cider,
-    }
-
-for dataname in datanames:
-    if dataname == 'VQA-MED':
-        continue
-    plt.plot(server_results[dataname]['samples'], server_results[dataname][plot_type], label=f'server-{dataname}', linewidth=2.0)
-
-for i in range(0, 3200, 200):
-    plt.axvline(x=i, color='b', linestyle='--', linewidth=0.1)
-
-plt.axvline(x=0, color='r', linestyle='--', linewidth=0.5)
-plt.axvline(x=800, color='r', linestyle='--', linewidth=0.5)
-plt.axvline(x=1600, color='r', linestyle='--', linewidth=0.5)
-plt.axvline(x=2400, color='r', linestyle='--', linewidth=0.5)
-plt.axvline(x=3200, color='r', linestyle='--', linewidth=0.5)
-
-plt.xlabel("# of samples", fontsize=20)
-plt.ylabel(plot_type, fontsize=20)
-plt.legend()
-plt.title(f"Method results - {plot_type}", fontsize=20)
-plt.savefig(f"{target_method_name}_results_plot - {plot_type}.png")
-plt.clf()
+    for i in range(num_rounds+1):
+        plt.axvline(x=i, color='r', linestyle='--', linewidth=0.5)
+    plt.xticks(list(range(num_rounds+1)))
+    plt.xlabel("# of rounds", fontsize=20)
+    plt.ylabel(plot_type, fontsize=20)
+    plt.legend()
+    plt.title(f"{Method} server - {plot_type}", fontsize=20)
+    plt.savefig(f"{Method}_server_{plot_type}.png")
+    
