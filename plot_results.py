8ad2b69c41a6b54f5781858829f91316f2bdb3f0
mounKim <nasmik419@gmail.com>
2024-05-06 18:41:51 +0900

    create csv file with  mean and final metric value

--- plot_results.py
+++ plot_results.py
@@ -3,12 +3,14 @@ import numpy as np
 import warnings
 import matplotlib.pyplot as plt
 import copy
+import csv
 warnings.filterwarnings(action='ignore')
 
-plot_types = ['rouge_l', 'recall', 'precision', 'bleu', 'meteor', 'rouge_l', 'cider']
+plot_types = ['recall', 'precision', 'bleu', 'meteor', 'rouge_l', 'cider']
 Method = 'fedavg_bunny3b'
 num_clients = 10
 num_rounds = 10
+do_plot = True
 
 target_dirs = [
     './eval_results/fedavg/fedavg_eval/round_1.log',
@@ -58,31 +60,70 @@ for round, filename in enumerate(target_dirs):
                 server[dataname]['cider'].append(float(line_s[33][1:-1]))
                 
 
+# csv file
+fields = ['name',
+    'mean_recall', 'final_recall',
+    'mean_precision', 'final_precision',
+    'mean_bleu', 'final_bleu',
+    'mean_meteor', 'final_meteor',
+    'mean_rouge_l', 'final_rouge_l',
+    'mean_cider', 'final_cider',
+]
+rows = [['client'],['server']] # one for cilent, one for server
 for plot_type in plot_types:
     # clients
-    plt.clf()
-    for client_id in range(num_clients):
-        plt.plot(clients[client_id]['rounds'], clients[client_id][plot_type], label=str(client_id), linewidth=2.0)
+    mean_per_client = [sum(clients[client_id][plot_type])/len(clients[client_id][plot_type]) for client_id in range(num_clients)]
+    final_per_client = [clients[client_id][plot_type][-1] for client_id in range(num_clients)]
+    rows[0].append(sum(mean_per_client)/len(mean_per_client))
+    rows[0].append(sum(final_per_client)/len(final_per_client))
     
-    for i in range(num_rounds+1):
-        plt.axvline(x=i, color='r', linestyle='--', linewidth=0.5)
-    plt.xticks(list(range(num_rounds+1)))
-    plt.xlabel("# of rounds", fontsize=20)
-    plt.ylabel(plot_type, fontsize=20)
-    plt.legend()
-    plt.title(f"{Method} clients - {plot_type}", fontsize=20)
-    plt.savefig(f"{Method}_clients_{plot_type}.png")
     # servr
-    plt.clf()
-    for dataname, server_info in server.items():
-        plt.plot(server_info['rounds'], server_info[plot_type], label=dataname, linewidth=2.0)
+    mean_per_server = [sum(server_info[plot_type])/len(server_info[plot_type]) for server_info in server.values()]
+    final_per_server = [server_info[plot_type][-1] for server_info in server.values()]
+    rows[1].append(sum(mean_per_server)/len(mean_per_server))
+    rows[1].append(sum(final_per_server)/len(final_per_server))
     
-    for i in range(num_rounds+1):
-        plt.axvline(x=i, color='r', linestyle='--', linewidth=0.5)
-    plt.xticks(list(range(num_rounds+1)))
-    plt.xlabel("# of rounds", fontsize=20)
-    plt.ylabel(plot_type, fontsize=20)
-    plt.legend()
-    plt.title(f"{Method} server - {plot_type}", fontsize=20)
-    plt.savefig(f"{Method}_server_{plot_type}.png")
+    # name of csv file
+    filename = f"{Method}.csv"
+    
+    # writing to csv file
+    with open(filename, 'w') as csvfile:
+        # creating a csv writer object
+        csvwriter = csv.writer(csvfile)
+    
+        # writing the fields
+        csvwriter.writerow(fields)
+    
+        # writing the data rows
+        csvwriter.writerows(rows)
+    
+    if do_plot:
+        # client
+        plt.clf()
+        for client_id in range(num_clients):
+            plt.plot(clients[client_id]['rounds'], clients[client_id][plot_type], label=str(client_id), linewidth=2.0)
+
+        
+        for i in range(num_rounds+1):
+            plt.axvline(x=i, color='r', linestyle='--', linewidth=0.5)
+        plt.xticks(list(range(num_rounds+1)))
+        plt.xlabel("# of rounds", fontsize=20)
+        plt.ylabel(plot_type, fontsize=20)
+        plt.legend()
+        plt.title(f"{Method} clients - {plot_type}", fontsize=20)
+        plt.savefig(f"{Method}_clients_{plot_type}.png")
+    
+        # server
+        plt.clf()
+        for dataname, server_info in server.items():
+            plt.plot(server_info['rounds'], server_info[plot_type], label=dataname, linewidth=2.0)
+        
+        for i in range(num_rounds+1):
+            plt.axvline(x=i, color='r', linestyle='--', linewidth=0.5)
+        plt.xticks(list(range(num_rounds+1)))
+        plt.xlabel("# of rounds", fontsize=20)
+        plt.ylabel(plot_type, fontsize=20)
+        plt.legend()
+        plt.title(f"{Method} server - {plot_type}", fontsize=20)
+        plt.savefig(f"{Method}_server_{plot_type}.png")
     
