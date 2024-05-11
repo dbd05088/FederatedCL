47b8af11a4659ce764fbdf6a05986c4e9b012126
mounKim <nasmik419@gmail.com>
2024-05-11 11:40:25 +0900

    preprocess mplug to remove invalid image data

--- preprocess_mPLUG.py
+++ preprocess_mPLUG.py
@@ -18,22 +18,42 @@ subset_folder = os.path.join(dir, 'test')
 if not os.path.exists(subset_folder):
     os.makedirs(subset_folder)
 
+invalid_file = open('./mPLUG_invalid.txt', 'r')
+invalid_img_files = invalid_file.readlines()
+invalid_file.close()
+for i in range(len(invalid_img_files)):
+    if i < len(invalid_img_files)-1:
+        invalid_img_files[i] = invalid_img_files[i][2:-1] 
+    else:
+        invalid_img_files[i] = invalid_img_files[i][2:] 
+
 print(tasks)
-idx = 0
+task_idx = 0
 for task in tasks:
     with open(task+'/train.json', 'r') as fp:
         train_json_data = json.load(fp)
     with open(task+'/test.json', 'r') as fp:
         test_json_data = json.load(fp)
-    for item in train_json_data:
-        if len(item['image']) == 0:
-            del item['image']
-    for item in test_json_data:
-        if len(item['image']) == 0:
-            del item['image']
-    with open(f'./dataset/mPLUG/train/dataset-{idx}.json', 'w') as json_file:
+
+    for jsondata in [train_json_data, test_json_data]:
+        idx_to_del = []
+        print(len(jsondata))
+        for idx in range(len(jsondata)):
+            if len(jsondata[idx]['image']) == 0:
+                del jsondata[idx]['image']
+            else:
+                for img in jsondata[idx]['image']:
+                    if img in invalid_img_files:
+                        print(img)
+                        idx_to_del.append(idx)
+                        break
+        for idx in idx_to_del:
+            del jsondata[idx]
+        print(len(jsondata))
+
+    with open(f'./dataset/mPLUG/train/dataset-{task_idx}.json', 'w') as json_file:
         json.dump(train_json_data, json_file, indent=4)
-    with open(f'./dataset/mPLUG/test/dataset-{idx}.json', 'w') as json_file:
+    with open(f'./dataset/mPLUG/test/dataset-{task_idx}.json', 'w') as json_file:
         json.dump(test_json_data, json_file, indent=4)
-    idx += 1
+    task_idx += 1
     
