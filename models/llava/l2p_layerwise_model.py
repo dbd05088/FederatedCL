f192705e1ef99b64a970d328150b65947629ed4e
cryingInitial <specific0924@gmail.com>
2024-07-22 14:49:54 +0900

    l2p layerwise fix error

--- models/llava/l2p_layerwise_model.py
+++ models/llava/l2p_layerwise_model.py
@@ -22,8 +22,11 @@ from transformers import AutoConfig, AutoModelForCausalLM, \
                          LlamaConfig, LlamaModel, LlamaForCausalLM
 from transformers.models.llama.modeling_llama import LlamaRMSNorm, LlamaDecoderLayer
 
-from transformers.modeling_outputs import CausalLMOutputWithPast
+from transformers.modeling_outputs import CausalLMOutputWithPast, BaseModelOutputWithPast
 from transformers.generation.utils import GenerateOutput
+from transformers.utils import logging
+from transformers.cache_utils import Cache, DynamicCache, StaticCache
+
 
 from models.llava.llava_arch import LlavaMetaModel, LlavaMetaForCausalLM
 
@@ -32,12 +35,16 @@ import warnings
 from models.llava.language_model.llava_llama import LlavaLlamaForCausalLM
 from models.llava.constants import IGNORE_INDEX, IMAGE_TOKEN_INDEX
 from models.prompt import Prompt
+import torch.nn.functional as F
+
+logger = logging.get_logger(__name__)
+
 
 class LlamaDecoderL2PLayer(LlamaDecoderLayer):
     def __init__(self, config: LlamaConfig, layer_idx: int):
         super().__init__(config, layer_idx)
         
-        self.lang_prompt = Prompt(length=20, top_k=5, pool_size=10, embed_dim=4096, key_dim=4096)
+        self.lang_prompt = Prompt(length=20, top_k=5, pool_size=10, embed_dim=config.hidden_size, key_dim=config.hidden_size)
 
     def forward(
         self,
@@ -48,6 +55,7 @@ class LlamaDecoderL2PLayer(LlamaDecoderLayer):
         output_attentions: Optional[bool] = False,
         use_cache: Optional[bool] = False,
         cache_position: Optional[torch.LongTensor] = None,
+        image_feature_indices=None,
         **kwargs,
     ) -> Tuple[torch.FloatTensor, Optional[Tuple[torch.FloatTensor, torch.FloatTensor]]]:
         """
@@ -70,26 +78,24 @@ class LlamaDecoderL2PLayer(LlamaDecoderLayer):
             )
             
         # add prompt
-        image_feature_indices = getattr(kwargs, 'image_feature_indices', None)
         bsz = hidden_states.shape[0]
         if image_feature_indices is not None:
             image_features = []
             for i in range(bsz):
-                breakpoint()
-                image_features.append(hidden_states[i,image_feature_indices, :])
-            image_features = torch.concat(image_features, dim=0)
-            image_features = image_features.mean(dim=1)
-            breakpoint()
+                image_features.append(hidden_states[i,image_feature_indices[i], :].mean(dim=0))
+                
+            image_features = torch.stack(image_features, dim=0)
             prompt_pool_output = self.lang_prompt(image_features)
             selected_prompts = prompt_pool_output['batched_prompt']
-            breakpoint()
             hidden_states = torch.concat((selected_prompts, hidden_states), dim=1)
             len_prompt = selected_prompts.shape[1]
+            
+            attention_mask = torch.concat((torch.full((bsz, selected_prompts.shape[1]), True).cuda(), attention_mask), dim=1)
+            position_ids = torch.arange(hidden_states.shape[1]).unsqueeze(0).cuda()
 
         residual = hidden_states
 
         hidden_states = self.input_layernorm(hidden_states)
-
 
         # Self Attention
         hidden_states, self_attn_weights, present_key_value = self.self_attn(
@@ -121,6 +127,9 @@ class LlamaDecoderL2PLayer(LlamaDecoderLayer):
 
         if use_cache:
             outputs += (present_key_value,)        
+            
+        if image_feature_indices is not None:
+            outputs += (prompt_pool_output['reduce_sim'],)
 
         return outputs
 class LlamaL2PModel(LlamaModel):
@@ -139,6 +148,127 @@ class LlamaL2PModel(LlamaModel):
         # Initialize weights and apply final processing
         self.post_init()
 
+    def forward(
+        self,
+        input_ids: torch.LongTensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+        cache_position: Optional[torch.LongTensor] = None,
+        image_feature_indices=None,
+    ) -> Union[Tuple, BaseModelOutputWithPast]:
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
+        use_cache = use_cache if use_cache is not None else self.config.use_cache
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        if (input_ids is None) ^ (inputs_embeds is not None):
+            raise ValueError(
+                "You cannot specify both input_ids and inputs_embeds at the same time, and must specify either one"
+            )
+
+        if self.gradient_checkpointing and self.training and use_cache:
+            logger.warning_once(
+                "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`."
+            )
+            use_cache = False
+
+        if inputs_embeds is None:
+            inputs_embeds = self.embed_tokens(input_ids)
+
+        past_seen_tokens = 0
+        if use_cache:  # kept for BC (cache positions)
+            if not isinstance(past_key_values, StaticCache):
+                past_key_values = DynamicCache.from_legacy_cache(past_key_values)
+                past_seen_tokens = past_key_values.get_seq_length()
+
+        if cache_position is None:
+            if isinstance(past_key_values, StaticCache):
+                raise ValueError("cache_position is a required argument when using StaticCache.")
+            cache_position = torch.arange(
+                past_seen_tokens, past_seen_tokens + inputs_embeds.shape[1], device=inputs_embeds.device
+            )
+
+        if position_ids is None:
+            position_ids = cache_position.unsqueeze(0)
+
+        causal_mask = self._update_causal_mask(attention_mask, inputs_embeds, cache_position, past_seen_tokens)
+
+        # embed positions
+        hidden_states = inputs_embeds
+
+        # decoder layers
+        all_hidden_states = () if output_hidden_states else None
+        all_self_attns = () if output_attentions else None
+        next_decoder_cache = None
+        
+        l2p_key_losses = 0
+        for decoder_layer in self.layers:
+            if output_hidden_states:
+                all_hidden_states += (hidden_states,)
+
+            if self.gradient_checkpointing and self.training:
+                layer_outputs = self._gradient_checkpointing_func(
+                    decoder_layer.__call__,
+                    hidden_states,
+                    causal_mask,
+                    position_ids,
+                    past_key_values,
+                    output_attentions,
+                    use_cache,
+                    cache_position,
+                    image_feature_indices,
+                )
+            else:
+                layer_outputs = decoder_layer(
+                    hidden_states,
+                    attention_mask=causal_mask,
+                    position_ids=position_ids,
+                    past_key_value=past_key_values,
+                    output_attentions=output_attentions,
+                    use_cache=use_cache,
+                    cache_position=cache_position,
+                    image_feature_indices=image_feature_indices,
+                )
+
+            hidden_states = layer_outputs[0]
+
+            if use_cache:
+                next_decoder_cache = layer_outputs[2 if output_attentions else 1]
+
+            if output_attentions:
+                all_self_attns += (layer_outputs[1],)
+            
+            if image_feature_indices is not None:
+                l2p_key_losses += layer_outputs[-1]
+
+        hidden_states = self.norm(hidden_states)
+
+        # add hidden states from the last decoder layer
+        if output_hidden_states:
+            all_hidden_states += (hidden_states,)
+
+        next_cache = None
+        if use_cache:
+            next_cache = (
+                next_decoder_cache.to_legacy_cache() if isinstance(next_decoder_cache, Cache) else next_decoder_cache
+            )
+        if not return_dict:
+            return tuple(v for v in [hidden_states, next_cache, all_hidden_states, all_self_attns] if v is not None)
+        return (BaseModelOutputWithPast(
+            last_hidden_state=hidden_states,
+            past_key_values=next_cache,
+            hidden_states=all_hidden_states,
+            attentions=all_self_attns,
+        ), l2p_key_losses)
+
 
 class LlavaConfig(LlamaConfig):
     model_type = "llava_llama"
@@ -151,10 +281,84 @@ class LlavaLlamaL2PModel(LlavaMetaModel, LlamaL2PModel):
         super(LlavaLlamaL2PModel, self).__init__(config)
 
 
-class LlavaLlamaL2PForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
+class LlamaL2pForCausalLM(LlamaForCausalLM):
+    def forward(
+        self,
+        input_ids: torch.LongTensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+        cache_position: Optional[torch.LongTensor] = None,
+        image_feature_indices=None,
+    ) -> Union[Tuple, CausalLMOutputWithPast]:
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        # decoder outputs consists of (dec_features, layer_state, dec_hidden, dec_attn)
+        outputs, l2p_key_losses = self.model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_values=past_key_values,
+            inputs_embeds=inputs_embeds,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+            cache_position=cache_position,
+            image_feature_indices=image_feature_indices,
+        )
+
+        hidden_states = outputs[0]
+        if self.config.pretraining_tp > 1:
+            lm_head_slices = self.lm_head.weight.split(self.vocab_size // self.config.pretraining_tp, dim=0)
+            logits = [F.linear(hidden_states, lm_head_slices[i]) for i in range(self.config.pretraining_tp)]
+            logits = torch.cat(logits, dim=-1)
+        else:
+            logits = self.lm_head(hidden_states)
+        logits = logits.float()
+
+        loss = None
+        if labels is not None:
+            # Shift so that tokens < n predict n
+            shift_logits = logits[..., :-1, :].contiguous()
+            shift_labels = labels[..., 1:].contiguous()
+            # Flatten the tokens
+            loss_fct = nn.CrossEntropyLoss()
+            shift_logits = shift_logits.view(-1, self.config.vocab_size)
+            shift_labels = shift_labels.view(-1)
+            # Enable model parallelism
+            shift_labels = shift_labels.to(shift_logits.device)
+            loss = loss_fct(shift_logits, shift_labels)
+            
+            loss -= 0.1*l2p_key_losses
+
+        if not return_dict:
+            output = (logits,) + outputs[1:]
+            return (loss,) + output if loss is not None else output
+
+        return CausalLMOutputWithPast(
+            loss=loss,
+            logits=logits,
+            past_key_values=outputs.past_key_values,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
+
+class LlavaLlamaL2PForCausalLM(LlamaL2pForCausalLM, LlavaMetaForCausalLM):
     config_class = LlavaConfig
 
-    def __init__(self, config):
+    def __init__(self, config, prompt_num):
         super(LlamaForCausalLM, self).__init__(config)
         self.model = LlavaLlamaL2PModel(config)
         self.pretraining_tp = config.pretraining_tp
