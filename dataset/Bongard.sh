05eb34d1e242fcd2cfe60e26794f3c6f08d306b8
carpedkm <flytodk@gmail.com>
2024-09-07 10:28:17 +0900

    update downloading

--- dataset/Bongard.sh
+++ dataset/Bongard.sh
@@ -4,8 +4,9 @@ rm Bongard-HOI.tar
 
 gdrive files download 1aXr3ihVq0mtzbl6ZNJMogYEyEY-WALNr
 unzip images.zip 
-mv images Bongard-OpenWorld
 git clone https://huggingface.co/datasets/joyjay/Bongard-OpenWorld
+mv images Bongard-OpenWorld
+rm images.zip
 cd Bongard-OpenWorld 
 rm -rf .git
 rm README.md
--- dataset/mPLUG.sh
+++ dataset/mPLUG.sh
@@ -1,4 +1,6 @@
 gdrive files download 1PMHwxw8mJ9UOm7q0SfXpGLu2wHEsbK1-
 tar -xvf M-Paper.tar
-# mv M-Paper mPLUG
+mv M-Paper mPLUG
 rm M-Paper.tar
+cd mPLUG
+mv imgs_sampled imgs
